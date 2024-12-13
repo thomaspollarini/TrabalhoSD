deeafb89912edbaf33ffc3b426c5883272b23072
Carlos Eduardo da Costa <costa25102002@gmail.com>
2024-12-13 11:23:15 -0300

    Implementando RMI no Banco de Dados

---
+++ TrabalhoSD/src/main/java/com/mycompany/trabalhoso/model/ModelAPI.java
@@ -0,0 +1,12 @@
+package com.mycompany.trabalhoso.model;
+import java.io.IOException;
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.util.List;
+public interface ModelAPI  extends Remote{
+    <T> boolean writeArq(T objeto, String caminhoArquivo) throws RemoteException;
+    List<Object> readArqCliente() throws RemoteException, IOException;
+    List<Conta> readArqConta() throws RemoteException ,IOException;
+    List<Transacao> readArqTransacao() throws RemoteException, IOException;
+
+}
