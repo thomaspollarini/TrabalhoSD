70c8056e222a187345b7c3fdbe63ec41a0aedf09
Thomas <thomassantospallarini@gmail.com>
2024-12-12 21:48:25 -0300

    Menu criar conta

--- TrabalhoSD/src/main/java/com/mycompany/trabalhoso/view/TrabalhoSO.java
+++ TrabalhoSD/src/main/java/com/mycompany/trabalhoso/view/TrabalhoSO.java
@@ -3,10 +3,9 @@ package com.mycompany.trabalhoso.view;
 import java.io.IOException;
 import java.util.Scanner;
 
-import com.mycompany.trabalhoso.control.ServiceCliente;
-import com.mycompany.trabalhoso.control.ServiceConta;
-import com.mycompany.trabalhoso.model.Cliente;
-import com.mycompany.trabalhoso.model.Conta;
+import com.mycompany.trabalhoso.control.*;
+import com.mycompany.trabalhoso.model.*;
+
 
 public class TrabalhoSO {
 
@@ -16,45 +15,44 @@ public class TrabalhoSO {
         cliente.setCliente(0, "pedro", "149.766.816-61");
 
         int opMenu = 0;
-        Scanner teclado = new Scanner(System.in);
+        try (Scanner teclado = new Scanner(System.in)) {
+            while(opMenu != 3){
 
-        while(opMenu != 3){
+                Conta c = new Conta();
+                Cliente cli = new Cliente();
 
-            Conta c = new Conta();
-            Cliente cli = new Cliente();
+                System.out.println("Banco Imobiliario");
+                System.out.println("1 - Logar Conta");
+                System.out.println("2 - Criar Conta");
+                System.out.println("3 - Sair");
+                opMenu = teclado.nextInt();
 
-            System.out.println("Banco Imobiliario");
-            System.out.println("1 - Logar Conta");
-            System.out.println("2 - Criar Conta");
-            System.out.println("3 - Sair");
-            opMenu = teclado.nextInt();
+                switch (opMenu) {
+                    case 1:
+                        System.out.println("login");
+                        System.out.println("Insira o identificador da conta:");
+                        System.out.println("Insira sua senha:");
 
-            switch (opMenu) {
-                case 1:
-                    System.out.println("login");
-                    System.out.println("Insira o identificador da conta:");
-                    System.out.println("Insira sua senha:");
+                        break;
+                    case 2:
+                        
+                        System.out.println("criar");
+                        
+                        System.out.println("Insira o identificador da sua conta:");
+                        c.setIdentificador(teclado.nextLine());
+                        System.out.println("Insira seu nome completo:");
+                        cli.setNome(teclado.nextLine());
+                        System.out.println("Insira seu CPF:");
+                        cli.setCPF(teclado.nextLine());
+                        System.out.println("Insira sua senha:");
+                        c.setSenha(teclado.nextLine());
 
-                    break;
-                case 2:
-                    
-                    System.out.println("criar");
-                    
-                    System.out.println("Insira o identificador da sua conta:");
-                    c.setIdentificador(teclado.nextLine());
-                    System.out.println("Insira seu nome completo:");
-                    cli.setNome(teclado.nextLine());
-                    System.out.println("Insira seu CPF:");
-                    cli.setCPF(teclado.nextLine());
-                    System.out.println("Insira sua senha:");
-                    c.setSenha(teclado.nextLine());
+                        System.out.println( SystemOperation.criarConta(cli,c));
 
-                    ServiceCliente.inserir(cli);
-                    ServiceConta.criarConta(c);
-
-                    break;
-                default:
-                    break;
+                        break;
+                    default:
+                        break;
+                }
             }
         }
     }
